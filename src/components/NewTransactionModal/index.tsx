13b72327b8ea06f7662c9b027a9ac96e2e3ebcd7
Calixoto <grimmjow006@live.com>
2021-05-19 12:19:52 -0300

    Visual Botões tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -24,11 +26,37 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             </button>
 
             <Container>
+
                 <h2>Cadastrar transação</h2>
+
                 <input placeholder="Titulo" />
+
                 <input type="number" placeholder="Valor" />
+
+                <TransactionTypeContainer>
+
+                    <button
+                        type="button"
+
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+
+                    <button
+                        type="button"
+
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+
+                </TransactionTypeContainer>
+
                 <input placeholder="Categoria" />
+
                 <button type="submit">Cadastrar</button>
+
             </Container>
         </Modal>
     )
