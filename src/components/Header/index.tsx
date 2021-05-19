67f9a5fb5ba631d8047fb8539903d4d08789c377
Calixoto <grimmjow006@live.com>
2021-05-19 10:59:15 -0300

    Criação Modal e uso de props básico

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import { Container, Content } from "./styles";
 import logoImg from '../../assets/logo.svg';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dtmoney" />
-                <button>Nova transação</button>
+                <button
+                    type="button"
+                    onClick={onOpenNewTransactionModal}
+                >
+                    Nova transação
+                    </button>
             </Content>
         </Container>
     )
